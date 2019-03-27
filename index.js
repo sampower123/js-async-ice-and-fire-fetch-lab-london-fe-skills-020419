977df6ea84db41fe829e3eaa8d1be410a2f3e47f
Sam Power <sam.g.power@gmail.com>
2019-03-27 19:37:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-rfunction fetchBooks() {
+function fetchBooks() {
   //write fetch request to the Game of Thrones API
   fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
